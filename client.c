350a8ee707d0b31048515bddb74ed601a4941a6b
Glenn Fiedler <glenn@networknext.com>
2024-07-05 17:15:56 -0400

    wip

--- client.c
+++ client.c
@@ -274,3 +274,28 @@ int main( int argc, char *argv[] )
 
     return 0;
 }
+
+
+// todo: don't really understand the ring buffer operations yet. study the code...
+/*
+    // Initialize umem frame allocation
+    for (i = 0; i < NUM_FRAMES; i++)
+        xsk_info->umem_frame_addr[i] = i * FRAME_SIZE;
+
+    xsk_info->umem_frame_free = NUM_FRAMES;
+
+    // Stuff the receive path with buffers, we assume we have enough
+    ret = xsk_ring_prod__reserve(&xsk_info->umem->fq,
+                     XSK_RING_PROD__DEFAULT_NUM_DESCS,
+                     &idx);
+
+    if (ret != XSK_RING_PROD__DEFAULT_NUM_DESCS)
+        goto error_exit;
+
+    for (i = 0; i < XSK_RING_PROD__DEFAULT_NUM_DESCS; i ++)
+        *xsk_ring_prod__fill_addr(&xsk_info->umem->fq, idx++) =
+            xsk_alloc_umem_frame(xsk_info);
+
+    xsk_ring_prod__submit(&xsk_info->umem->fq,
+                  XSK_RING_PROD__DEFAULT_NUM_DESCS);
+*/
