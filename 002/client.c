e8ed16c13b6f8102c27fac75968c4234ae0e273f
Glenn Fiedler <glenn@networknext.com>
2024-07-28 11:28:30 -0400

    build it up

--- 002/client.c
+++ 002/client.c
@@ -214,7 +214,7 @@ int client_init( struct client_t * client, const char * interface_name )
         xsk_config.rx_size = 0;
         xsk_config.tx_size = XSK_RING_PROD__DEFAULT_NUM_DESCS;
         xsk_config.xdp_flags = 0;
-        xsk_config.bind_flags = 09;
+        xsk_config.bind_flags = 0;
         xsk_config.libbpf_flags = XSK_LIBBPF_FLAGS__INHIBIT_PROG_LOAD;
 
         int queue_id = i;
